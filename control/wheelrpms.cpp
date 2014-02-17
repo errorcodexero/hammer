6c58c26471be1447dae2156f7f03b764eb631815
Adrian <earthvexingtimelord@gmail.com>
2014-02-17 14:09:29 -0800

    created wheelrpm stuff-add defaults

--- control/wheelrpms.cpp
+++ control/wheelrpms.cpp
@@ -1,25 +1,13 @@
+#include "wheelrpms.h"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unistd.h>
 #include <stdlib.h>
 #include <assert.h>
 #include "shooter_wheels.h"
 
 using namespace std;
-
-struct wheelcalib{
-	Shooter_wheels::Status highgoal;
-	Shooter_wheels::Status lowgoal;
-	Shooter_wheels::Status overtruss;
-	Shooter_wheels::Status passing;
-};
-
-struct crow{
-	Shooter_wheels::Status newhighgoal;
-	Shooter_wheels::Status newlowgoal;
-	Shooter_wheels::Status newovertruss;
-	Shooter_wheels::Status newpassing;
-};
 
 ostream& operator<<(ostream& o,wheelcalib w){
 	o<<"High Goal RPMs(Top)	="<<w.highgoal.top<<endl;
@@ -45,19 +33,35 @@ string postequalstr(string s){
 	return aft;
 }
 
-int main(){	
+void configfile(){
+	ofstream wheelrpms;
+	wheelrpms.open("wheelrpms.txt");
+	wheelrpms<<"Shooter Wheel RPMs:"<<endl<<rpmsdefault()<<endl;
+	wheelrpms.close();
+}
+
+wheelcalib rpmsdefault(){
 	wheelcalib rpms;
-	wheelcalib newcrow;
 	rpms.highgoal=Shooter_wheels::Status(1200,3205);
 	rpms.lowgoal=Shooter_wheels::Status(3999,1);
 	rpms.overtruss=Shooter_wheels::Status(12,532);
 	rpms.passing=Shooter_wheels::Status(3025,1982);
+	return rpms;
+}
+
+void adddefaultrpms(){
+	wheelcalib rpms;
+	rpms=rpmsdefault();
 	ofstream wheelrpms;
-	wheelrpms.open("wheelspeeds.txt");
+	wheelrpms.open("wheelrpms.txt");
 	wheelrpms<<"Shooter Wheel RPMs:"<<endl<<rpms<<endl;
 	wheelrpms.close();
+}
+
+wheelcalib readconfig(){
+	wheelcalib newcrow;
 	string s;
-	ifstream f("wheelspeeds.txt");
+	ifstream f("wheelrpms.txt");
 	int c=1;
 	while(f.good()){
 		getline(f,s);
@@ -74,6 +78,58 @@ int main(){
 		c++;	
 	}
 	f.close();
+	return newcrow;
+}
+
+void writeconfig(wheelcalib input){
+	wheelcalib rpms;
+	rpms.highgoal.top=input.highgoal.top;
+	rpms.highgoal.bottom=input.highgoal.bottom;
+	rpms.lowgoal.top=input.lowgoal.top;
+	rpms.lowgoal.bottom=input.lowgoal.bottom;
+	rpms.overtruss.top=input.overtruss.top;
+	rpms.overtruss.bottom=input.overtruss.bottom;
+	rpms.passing.top=input.passing.top;
+	rpms.passing.bottom=input.passing.bottom;
+	ofstream wheelrpms;
+	wheelrpms.open("wheelrpms.txt");
+	wheelrpms<<"Shooter Wheel RPMs:"<<endl<<rpms<<endl;
+	wheelrpms.close();
+}
+
+#ifdef WHEEL_RPMS_TEST
+int main(){	
+	wheelcalib rpms;
+	wheelcalib newcrow;
+	rpms=rpmsdefault();
+	ofstream wheelrpms;
+	unlink("wheelrpms.txt");
+	configfile();
+	adddefaultrpms();
+	wheelrpms.open("wheelrpms.txt");
+	wheelrpms<<"Shooter Wheel RPMs:"<<endl<<rpms<<endl;
+	wheelrpms.close();
+	string s;
+	ifstream f("wheelrpms.txt");
+	int c=1;
+	while(f.good()){
+		getline(f,s);
+		if(whereequal(s)!=-1){
+			if(c==2)newcrow.highgoal.top=atoi(postequalstr(s).c_str());
+			if(c==3)newcrow.highgoal.bottom=atoi(postequalstr(s).c_str());
+			if(c==4)newcrow.lowgoal.top=atoi(postequalstr(s).c_str());
+			if(c==5)newcrow.lowgoal.bottom=atoi(postequalstr(s).c_str());
+			if(c==6)newcrow.overtruss.top=atoi(postequalstr(s).c_str());
+			if(c==7)newcrow.overtruss.bottom=atoi(postequalstr(s).c_str());
+			if(c==8)newcrow.passing.top=atoi(postequalstr(s).c_str());
+			if(c==9)newcrow.passing.bottom=atoi(postequalstr(s).c_str())+6;
+		}
+		c++;	
+	}
+	f.close();
 	cout<<newcrow;
+	writeconfig(newcrow);
+	cout<<readconfig();
 	return 0;
 }
+#endif

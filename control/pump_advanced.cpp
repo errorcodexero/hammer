ff6e6ae883c9ded6e5266736912b79cc2b699846
ripster <rippey.e>
2014-03-14 13:17:33 -0700

    Improve air use estimates

--- control/pump_advanced.cpp
+++ control/pump_advanced.cpp
@@ -7,6 +7,41 @@ Use the pressure estimates to control the pump at your own risk.  As far as I ca
 
 using namespace std;
 
+double square(double x){ return x*x; }
+static const double PI=3.14159;
+
+typedef double Inch;
+typedef double In3;
+
+In3 cylinder_volume(Inch bore,Inch stroke){
+	//ignoring the size of the shaft
+	return PI*square(bore/2)*stroke;
+}
+
+//use constexpr when available
+In3 injector_volume(){
+	return 2*cylinder_volume(.75,8); //sizes are total guesses
+}
+
+In3 injector_arms_volume(){
+	return cylinder_volume(.5,3);
+}
+
+In3 ejector_volume(){
+	return 2*cylinder_volume(.5,3);
+}
+
+In3 collector_tilt_volume(){
+	return 2*cylinder_volume(2,4);
+}
+
+In3 tank_volume(){
+	//am-2477 per the AndyMark website, (.5 L); we should double check that this is what we're using.
+	static const In3 SINGLE_TANK=30.5;
+
+	return SINGLE_TANK*3;
+}
+
 namespace Pump_advanced{
 	static const PSI SWITCH_CHANGE_LOW=85;
 	static const PSI SWITCH_CHANGE_HIGH=110;
@@ -34,28 +69,28 @@ namespace Pump_advanced{
 		if(last_time==-1) last_time=now;
 		//cout<<"ch:"<<collector_tilt_out<<collector_tilt<<"\n";
 
-		PSI air_use=0;
+		In3 air_use=0;
 		if(collector_tilt_out!=collector_tilt){
-			air_use+=5;//totally made up constant
+			air_use+=collector_tilt_volume();
 			cout<<"c\n";
 			collector_tilt=collector_tilt_out;
 			cout<<"d\n";
 		}
 		if(injector_out!=injector){
-			air_use+=4;
+			air_use+=injector_volume();
 			injector=injector_out;
 		}
 		if(injector_arms_out!=injector_arms){
-			air_use+=1;
+			air_use+=injector_arms_volume();
 			injector_arms=injector_arms_out;
 		}
 		if(ejector_out!=ejector){
-			air_use+=1;
+			air_use+=ejector_volume();
 			ejector=ejector_out;
 		}
 		//could add something here to account for leaks in the system.
 
-		psi-=air_use;
+		psi-=60*(air_use/tank_volume());
 		if(psi<0) psi=0;
 
 		//cout<<"m="<<psi<<"\n";
@@ -138,8 +173,19 @@ void switch_sim_test(){
 	}
 }
 
+void print_volumes(){
+	#define X(name) cout<<""#name<<":"<<name()<<"\n";
+	X(injector_volume)
+	X(injector_arms_volume)
+	X(ejector_volume)
+	X(collector_tilt_volume)
+	X(tank_volume)
+	#undef X
+}
+
 int main(){
 	//switch_sim_test();
 	est_test();
+	//print_volumes();
 }
 #endif
